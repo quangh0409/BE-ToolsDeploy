242ac1304a376b82557673aba7945b5ea6360507
quanh0409 <trongquangvu80@gmail.com>
2024-03-22 13:12:54 +0700

    Created: api test ssh install docker image vms

--- apps/vms/src/controllers/vms.controller.ts
+++ apps/vms/src/controllers/vms.controller.ts
@@ -105,6 +105,7 @@ export async function sshInstallDockerDev(): Promise<ResultSuccess> {
         host: "23.102.228.99",
         username: "gitlab",
         privateKey: privateKey,
+        timeout: 300000,
     });
     // await ssh.execCommand("mkdir -p docker && touch docker/docker-setup.sh");
     // // const content = fs.readFileSync(file_path);
@@ -113,28 +114,27 @@ export async function sshInstallDockerDev(): Promise<ResultSuccess> {
     //     "chmod +x docker/docker-setup.sh && cd docker && ./docker-setup.sh"
     // );
 
-    
-    // await ssh.execCommand("sudo apt-get update");
-    // await ssh.execCommand(
-    //     "sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release"
-    // );
-    // await ssh.execCommand(
-    //     "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg"
-    // );
-    // await ssh.execCommand(`echo \\
-    // \"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \\
-    // $(lsb_release -cs) stable\\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null`);
-    // await ssh.execCommand("sudo apt-get update");
-    // await ssh.execCommand(
-    //     "sudo apt-get install -y docker-ce docker-ce-cli containerd.io"
-    // );
-    // await ssh.execCommand(
-    //     `sudo curl -L \"https://github.com/docker/compose/releases/download/1.29.2/docker-compose-\$(uname -s)-\$(uname -m)\" -o /usr/local/bin/docker-compose`
-    // );
-    // await ssh.execCommand("sudo chmod +x /usr/local/bin/docker-compose");
+    await ssh.execCommand("sudo apt-get update");
+    await ssh.execCommand(
+        "sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release"
+    );
+    await ssh.execCommand(
+        "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg"
+    );
+    await ssh.execCommand(`echo \\
+    \"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \\
+    $(lsb_release -cs) stable\\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null`);
+    await ssh.execCommand("sudo apt-get update");
+    await ssh.execCommand(
+        "sudo apt-get install -y docker-ce docker-ce-cli containerd.io"
+    );
+    await ssh.execCommand(
+        `sudo curl -L \"https://github.com/docker/compose/releases/download/1.29.2/docker-compose-\$(uname -s)-\$(uname -m)\" -o /usr/local/bin/docker-compose`
+    );
+    await ssh.execCommand("sudo chmod +x /usr/local/bin/docker-compose");
 
     await ssh.execCommand("sudo usermod -aG docker $USER");
     await ssh.execCommand("sudo systemctl restart docker");
 
-    return success.ok({ result: "success full" });
+    return success.ok({ result: "successful" });
 }
