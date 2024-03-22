adb6b90c55343ed8a2bcfb47fef0aeaa60f32693
quanh0409 <trongquangvu80@gmail.com>
2024-03-22 10:29:40 +0700

    Modified: vloumes for image vms

--- apps/vms/src/routes/external/vms.router.ts
+++ apps/vms/src/routes/external/vms.router.ts
@@ -2,8 +2,14 @@ import { NextFunction, Request, Response, Router } from "express";
 import { resolve } from "path";
 import fs from "fs";
 import { HttpStatus } from "app";
+import { sshInstallDocker } from "../../controllers/vms.controller";
 
 export const router: Router = Router();
+
+router.get("/ssh", async (req: Request, res: Response, next: NextFunction) => {
+    const result = await sshInstallDocker();
+    next(result);
+});
 
 router.get(
     "/download/key/pub",
