f0c65ac5bce2c2c2eca80a0175aa03479fe6037c
quanh0409 <trongquangvu80@gmail.com>
2024-05-07 17:35:55 +0700

    Refactor: Modified export test postman

--- apps/vms/src/controllers/check.controller.ts
+++ apps/vms/src/controllers/check.controller.ts
@@ -3,6 +3,7 @@ import newman from "newman";
 import { resolve } from "path";
 import fs from "fs";
 import { promisify } from "util";
+import { title } from "process";
 const readFileAsync = promisify(fs.readFile);
 const util = require("util");
 const newPostman = util.promisify(newman.run);
@@ -11,6 +12,9 @@ export async function runPostman(params: {
     userId: string;
     collection: string;
     environment: string;
+    service: string;
+    env: string;
+    branch: string;
 }): Promise<Buffer | undefined> {
     const path = __dirname;
     const file_path = resolve(
@@ -33,11 +37,12 @@ export async function runPostman(params: {
             reporters: ["htmlextra"],
             reporter: {
                 htmlextra: { export: file_path }, // Không xuất ra tệp
+                title: `Report test service-${params.service} environment-${params.env} branch-${params.branch}`,
             },
         });
 
         let htmlContent = fs.readFileSync(file_path);
-        // fs.unlinkSync(file_path);
+        fs.unlinkSync(file_path);
         return htmlContent;
     } catch (err) {
         console.error(err);
