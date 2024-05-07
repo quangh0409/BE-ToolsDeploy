9b25e99de4e4ba9210f55a2ed34aba6ca0f88f2e
quanh0409 <trongquangvu80@gmail.com>
2024-05-07 17:39:21 +0700

    Refactor: Modified paramater of api export postman

--- apps/vms/src/routes/external/postman.router.ts
+++ apps/vms/src/routes/external/postman.router.ts
@@ -7,8 +7,18 @@ export const router: Router = Router();
 router.post("/", async (req: Request, res: Response, next: NextFunction) => {
     const collection = req.body.collection as string;
     const environment = req.body.environment as string;
+    const service = req.body.service as string;
+    const branch = req.body.branch as string;
+    const env = req.body.env as string;
     const { id: userId } = req.payload as Payload;
-    const result = await runPostman({ userId, collection, environment });
+    const result = await runPostman({
+        userId,
+        collection,
+        environment,
+        service,
+        env,
+        branch,
+    });
     res.setHeader("Content-Type", "text/html");
 
     res.status(HttpStatus.OK).send(result).end();
